222e52e5e88461eafcdfec7bc9fbeb8ad4bb6586
George <paraschosg872@gmail.com>
2025-01-19 15:31:09 +0200

    first commit

---
+++ src/test/java/org/example/portfolioweb/PortfolioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.portfolioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
